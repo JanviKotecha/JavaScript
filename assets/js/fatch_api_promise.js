2b00b949842f3a9b060d889b20ca8ee15720d94f
janvi <janvi.kotecha0@gmail.com>
2022-01-08 16:40:05 +0530

    image

--- assets/js/fatch_api_promise.js
+++ assets/js/fatch_api_promise.js
@@ -9,15 +9,23 @@ function getRecord() {
 
       var opt = document.getElementById('tbd');
       data.forEach(function (e) {
-        opt.innerHTML += `<tr>
-            <td>${e.id}</td>
-            <td>${e.name}</td>
-            <td>${e.username}</td>
-            <td>${e.email}</td>
-            <td>${e.phone}</td>
-            <td>${e.website}</td>
-            
-            <td><a href=fatch_api_view.html?id=${e.id}>Show</td>`;
+        opt.innerHTML += `
+        <div class="row">
+          <div class="col-sm-12">
+              <div class="card">
+                <div class="card-body">
+                  <p>Id : ${e.id}</p>
+                  <p>Name :${e.name}</p>
+                  <p>Usermae :${e.username}</p>
+                  <p>Email : ${e.email}</p>
+                  <p>Phone :${e.phone}</p>
+                  <p>Website :${e.website}</p><hr>
+                  <a href=fatch_api_view.html?id=${e.id} class='btn btn-primary'>Post
+                  <a href=image_api.html?id=${e.id} class='btn btn-success'>Photos</a>
+                  <hr><br>
+                </div>
+              </div>
+            </div>`;
       });
     });
 }
---
+++ assets/js/image_api.js
@@ -0,0 +1,34 @@
+var urlst = window.location.href;
+const url = new URL(urlst);
+
+const id = url.searchParams.get('id');
+console.log(id);
+
+function viewPhoto() {
+  console.log('phptos');
+  console.log(id);
+  var url = `https://jsonplaceholder.typicode.com/photos?albumId=${id}`;
+
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+
+      var opt = document.getElementById('div13');
+      data.forEach(function (e) {
+        opt.innerHTML += `
+        <div class="row">
+          <div class="col-sm-">
+              <div class="card">
+                  <img src="${e.url}" style="width:120px;hight:120px">
+                  <h5>${e.title}<h5>
+                  <a href="${e.thumbnailUrl}">ShowThumb</a>
+              </div>
+            </div>
+          </div>`;
+      });
+    });
+}
+viewPhoto();
